48aecbc02cd71b2cda94bc802f4bda6a30654bfe
SergioEG <sergioeg19@hotmail.com>
2022-06-07 09:40:26 -0600

    Reflex Agent In HTML
    Reflex Agent in HTMl

--- src/index.js
+++ src/index.js
@@ -25,37 +25,83 @@ const timeSleep = (time) => {
 
 };
 
-const paintHmtl = (cleanerPos, action_result, aBox, bBox) => {
+// Paint HTML
+const paintHtml = (cleanerPos, action_result, aBox, bBox) => {
 
   document.getElementById("log").innerHTML += "<br>Cleaner in: "
     .concat(cleanerPos)
     .concat(" | Action: ")
-    .concat(action_result)
-    .concat(" | State: ")
-    .concat(stateNumber);
+    .concat(action_result);
+
+};
+
+// Stop Cycle
+const stopCycle = (states) => {
+
+   if(states[1] == "CLEAN" && states[2] == "CLEAN"){
+	
+   	document.getElementById("log").innerHTML += "<br>------- State End -------"; 
+	
+	return false;
+
+   } else {
+		
+	return false;
+	
+   }
+
+}
+
+// Dirty State
+// 0 = nada [00, 10]
+// 1 = dirty A   [11, 30]
+// 2 = dirty B   [31, 50]
+const randomDirty = (max, min) => {
+
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  if(randomNumber >= 0 && randomNumber < 11) {
+
+	return 0;
+
+  } else if(randomNumber >= 11 && randomNumber < 31) { 
+
+	return 1;
+
+  } else {
+  
+	return 2;
+
+  }
 
 };
 
 // Main
 const test = async(states) => {
 
-  let location = states[0];
-  let state = states[0] == "A" ? states[1] : states[2];
-  let action_result = reflex_agent(location, state);
+  let flag = true;
 
-  paintHmtl(location, action_result, states[1], states[2]);
+  while(flag) {
 
-  if(action_result == "CLEAN") {
+      let location = states[0];
+  	let state = states[0] == "A" ? states[1] : states[2];
+  	let action_result = reflex_agent(location, state);
 
-  	if(location == "A") {
+  	paintHtml(location, action_result, states[1], states[2]);
+
+  	if(action_result == "CLEAN") {
+
+  		if(location == "A") {
  
-      	states[1] = "CLEAN";
+      		states[1] = "CLEAN";
 
-      } else if (location == "B") {
+      	} else if (location == "B") {
 
-		states[2] = "CLEAN";
+			states[2] = "CLEAN";
 
-      } else if (action_result == "RIGHT") {
+      	} 
+
+  	} else if (action_result == "RIGHT") {
 
 		states[0] = "B";
 
@@ -65,11 +111,27 @@ const test = async(states) => {
 
       }
  
-      // Sleep
-      await timeSleep(1000);
+  	// Sleep
+  	await timeSleep(1000);
+
+      const dirty = randomDirty(0, 50);
+
+	if(dirty != 0) {
+
+		states[dirty] = "DIRTY"
+
+	}
+	console.log(states);
+	if(stopCycle(states)) {
+
+		flag = false;
+
+	}
+
+  }
 
 };
 
 // Init
-var states = ["A", "DIRTY", "DIRTY"];
+let states = ["A", "DIRTY", "DIRTY"];
 test(states);
